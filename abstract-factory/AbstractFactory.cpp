67468dfe45dc13f3bf6b20f5a3a0b47d56998303
Sandesh J Ghimire <sandesh@soccentric.com>
2019-10-19 11:41:37 -0700

    Update AbstractFactory.cpp
    upgrade to C++ 17

--- abstract-factory/AbstractFactory.cpp
+++ abstract-factory/AbstractFactory.cpp
@@ -15,41 +15,36 @@
  * products implement the same interface so that the classes can refer
  * to the interface not the concrete product
  */
-class ProductA
-{
+class ProductA {
 public:
-  virtual ~ProductA() {}
-  
-  virtual const char* getName() = 0;
-  // ...
+    virtual ~ProductA() = default;
+
+    virtual const char *getName() = 0;
+    // ...
 };
 
 /*
  * ConcreteProductAX and ConcreteProductAY
  * define objects to be created by concrete factory
  */
-class ConcreteProductAX : public ProductA
-{
+class ConcreteProductAX : public ProductA {
 public:
-  ~ConcreteProductAX() {}
-  
-  const char* getName()
-  {
-    return "A-X";
-  }
-  // ...
+    ~ConcreteProductAX() override = default;
+
+    const char *getName() override {
+        return "A-X";
+    }
+    // ...
 };
 
-class ConcreteProductAY : public ProductA
-{
+class ConcreteProductAY : public ProductA {
 public:
-  ~ConcreteProductAY() {}
-  
-  const char* getName()
-  {
-    return "A-Y";
-  }
-  // ...
+    ~ConcreteProductAY() override = default;
+
+    const char *getName() override {
+        return "A-Y";
+    }
+    // ...
 };
 
 /*
@@ -57,54 +52,49 @@ public:
  * same as Product A, Product B declares interface for concrete products
  * where each can produce an entire set of products
  */
-class ProductB
-{
+class ProductB {
 public:
-  virtual ~ProductB() {}
-  
-  virtual const char* getName() = 0;
-  // ...
+    virtual ~ProductB() = default;
+
+    virtual const char *getName() = 0;
+    // ...
 };
 
 /*
  * ConcreteProductBX and ConcreteProductBY
  * same as previous concrete product classes
  */
-class ConcreteProductBX : public ProductB
-{
+class ConcreteProductBX : public ProductB {
 public:
-  ~ConcreteProductBX() {}
-  
-  const char* getName()
-  {
-    return "B-X";
-  }
-  // ...
+    ~ConcreteProductBX() override = default;
+
+    const char *getName() override {
+        return "B-X";
+    }
+    // ...
 };
 
-class ConcreteProductBY : public ProductB
-{
+class ConcreteProductBY : public ProductB {
 public:
-  ~ConcreteProductBY() {}
-  
-  const char* getName()
-  {
-    return "B-Y";
-  }
-  // ...
+    ~ConcreteProductBY() override = default;
+
+    const char *getName() override {
+        return "B-Y";
+    }
+    // ...
 };
 
 /*
  * Abstract Factory
  * provides an abstract interface for creating a family of products
  */
-class AbstractFactory
-{
+class AbstractFactory {
 public:
-  virtual ~AbstractFactory() {}
-  
-  virtual ProductA *createProductA() = 0;
-  virtual ProductB *createProductB() = 0;
+    virtual ~AbstractFactory() = default;
+
+    virtual ProductA *createProductA() = 0;
+
+    virtual ProductB *createProductB() = 0;
 };
 
 /*
@@ -112,55 +102,50 @@ public:
  * each concrete factory create a family of products and client uses
  * one of these factories so it never has to instantiate a product object
  */
-class ConcreteFactoryX : public AbstractFactory
-{
+class ConcreteFactoryX : public AbstractFactory {
 public:
-  ~ConcreteFactoryX() {}
-  
-  ProductA *createProductA()
-  {
-    return new ConcreteProductAX();
-  }
-  ProductB *createProductB()
-  {
-    return new ConcreteProductBX();
-  }
-  // ...
+    ~ConcreteFactoryX() override = default;
+
+    ProductA *createProductA() override {
+        return new ConcreteProductAX();
+    }
+
+    ProductB *createProductB() override {
+        return new ConcreteProductBX();
+    }
+    // ...
 };
 
-class ConcreteFactoryY : public AbstractFactory
-{
+class ConcreteFactoryY : public AbstractFactory {
 public:
-  ~ConcreteFactoryY() {}
+    ~ConcreteFactoryY() override = default;
 
-  ProductA *createProductA()
-  {
-    return new ConcreteProductAY();
-  }
-  ProductB *createProductB()
-  {
-    return new ConcreteProductBY();
-  }
-  // ...
+    ProductA *createProductA() override {
+        return new ConcreteProductAY();
+    }
+
+    ProductB *createProductB() override {
+        return new ConcreteProductBY();
+    }
+    // ...
 };
 
 
-int main()
-{
-  ConcreteFactoryX *factoryX = new ConcreteFactoryX();
-  ConcreteFactoryY *factoryY = new ConcreteFactoryY();
+int main() {
+    auto *factoryX = new ConcreteFactoryX();
+    auto *factoryY = new ConcreteFactoryY();
 
-  ProductA *p1 = factoryX->createProductA();
-  std::cout << "Product: " << p1->getName() << std::endl;
-  
-  ProductA *p2 = factoryY->createProductA();
-  std::cout << "Product: " << p2->getName() << std::endl;
-  
-  delete p1;
-  delete p2;
-  
-  delete factoryX;
-  delete factoryY;
-  
-  return 0;
+    ProductA *p1 = factoryX->createProductA();
+    std::cout << "Product: " << p1->getName() << std::endl;
+
+    ProductA *p2 = factoryY->createProductA();
+    std::cout << "Product: " << p2->getName() << std::endl;
+
+    delete p1;
+    delete p2;
+
+    delete factoryX;
+    delete factoryY;
+
+    return 0;
 }
